d84619b3400f813cdd69af6ddecb604a56e51671
wwwsec <29392964@qq.com>
2018-09-24 10:12:02 +0800

    add redis

--- env.php
+++ env.php
@@ -4,4 +4,11 @@
 // foreach ($_ENV as $key => $value) {
 //   echo "{$key}: {$value}\n";
 // }
-phpinfo();
+// phpinfo();s
+$redis=new \Redis();
+$redis->connect("106.12.95.247", 6379); //连接Redis
+// $redis->auth($_ENV["REDIS_PASS"]); //密码验证
+$redis->select(1);//选择数据库2
+$redis->set( "code", "11111111111"); //设置测试key
+$redis->expire("11111111111",300); 
+echo $redis->get("11111111111");//输出value
